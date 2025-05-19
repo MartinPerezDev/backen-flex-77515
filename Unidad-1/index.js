5375e140e4021159db282450fba6d225b600bc0d
MartinPerezDev <martinperez9713@gmail.com>
2025-05-19 12:32:29 -0300

    Unidad 1 - repaso y Class

---
+++ Unidad-1/index.js
@@ -0,0 +1,37 @@
+let nombre = "Facundo";
+
+let id = 2;
+
+//Boolean
+let administrador = false;
+
+//Null
+let usuario = null;
+
+//Undefined
+let data;
+
+let fecha = new Date().toLocaleString();
+
+function authUser() {};
+
+class User {};
+
+const estudiantes = ["Constanza", "Carlos", "Franco"];
+
+const estudiante = {
+  nombre : "Constanza",
+  curso: "Backend 1"
+}
+
+//console.log( estudiante.curso );
+
+function sumar(num1, num2) {
+  return num1 + num2;
+}
+
+sumar(10, 20);
+
+const sumarArrowFunction = (num1, num2) => num1 + num2;
+
+sumarArrowFunction(10, 20);
